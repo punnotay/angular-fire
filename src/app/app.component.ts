d536fe43582ba14d29de39a0cc72c824a2251a60
Nattaporn Punnotayan <nattapornpunnotayan@Nattaporns-MacBook-Pro.local>
2017-05-05 19:57:49 -0500

    fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
 import { AngularFireDatabase,  
         FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 
 @Component({
@@ -11,7 +14,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class AppComponent {
   items: FirebaseListObservable<any[]>;
-  name: any;
+  name: Observable<firebase.User>;
   msgVal: string = '';
 
   constructor(public af: AngularFireAuth, public db: AngularFireDatabase) {
@@ -20,11 +23,15 @@ export class AppComponent {
         limitToLast: 50
       }
     });
-    this.name = af.auth.signInAnonymously();
+    this.name = this.af.authState;
   }
 
   login() {
     this.af.auth.signInAnonymously();
+  }
+
+  logout() {
+    this.af.auth.signOut();
   }
 
   Send(desc: string) {
